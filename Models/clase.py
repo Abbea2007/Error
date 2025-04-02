e0c72d7dda56912ebb1383264ad80c9a8bf0d644
Carlos Abea <carlosabea91@gmail.com>
2025-04-02 08:13:22 -0600

    Primer commit

---
+++ Models/clase.py
@@ -0,0 +1,8 @@
+class Materia():
+    def __init__(self, nombre, codigo, creditos):
+        self.nombre = nombre
+        self.codigo = codigo
+        self.creditos = creditos
+
+    def __str__(self):
+        return f"{self.nombre} ({self.codigo}) - {self.creditos} creditos"
---
+++ principal.py
@@ -0,0 +1,11 @@
+import Models.clase as c
+import controllers.dao_controller as dao
+
+materia = c.Materia("Calculo", "MAT101", 4)
+
+dao = dao.MateriaDao()
+dao.agregar_materia(materia)
+dao.obtener_materias()
+
+
+    
